822375649c5ca1bf5873f1358250b3f44bb0993d
ManWithTheCode <113933694+ManWithTheCode@users.noreply.github.com>
2022-10-14 13:57:01 -0400

    Update SimonSays.py

--- SimonSays.py
+++ SimonSays.py
@@ -1,4 +1,16 @@
 # Simon-Says Coding Attempt
+import time
+import board
+import touchio
+import digitalio as dio
 
+touch_pad = board.A1
+touch = touchio.TouchIn(touch_pad)
+led = dio.DigitalInOut(board.LED)
+led.direction = dio.Direction.OUTPUT 
 my_list = [red, green, blue, yellow]
-my_list.append(random
+my_list.append(random.randint(0, 4))
+
+while True:
+    
+  print(my_list)
