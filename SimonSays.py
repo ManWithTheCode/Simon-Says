94985218a2e1d8281429ca5b8882a3b45dcbcd1a
ManWithTheCode <113933694+ManWithTheCode@users.noreply.github.com>
2022-10-19 13:56:39 -0400

    Update SimonSays.py

--- SimonSays.py
+++ SimonSays.py
@@ -1,20 +1,49 @@
-# Simon-Says Coding Attempt
 import time
 import board
 import touchio
 import digitalio as dio
-import neopixel
+import random
 
-my_list = [red, green, blue, yellow]
-def simon_says(my_list):
-  rgb = [255, 0, 0]
-  Touch1 = rgb
-  
+my_list = ['red', 'green', 'blue', 'yellow']
+
+def fade_out(color, increment):
+    rgb = color
+    maximum = max (rgb[0], max(rgb[1], rgb[2]))
+    r_inc = rgb[0] / maximum
+    g_inc = rgb[1] / maximum
+    b_inc = rgb[2] / maximum
+    r, g, b = rgb
+    while r >= 0 and g >= 0 and b >= 0:
+        r -= r_inc
+        g -= g_inc
+        b -= b_inc
+        np.fill((int(r), int(g), int(b)))
+        np.show()
+        time.sleep(increment)
+def fade_in(color, increment):
+    rgb = color
+    r, g, b = rgb
+    r_final = r
+    g_final = g
+    b_final = b
+    print(r_final, g_final, b_final)
+    maximum = max(rgb[0], max(rgb[1], rgb[2]))
+    r_inc = rgb[0] / maximum
+    g_inc = rgb[1] / maximum
+    b_inc = rgb[2] / maximum
+    r, g, b = 0, 0, 0
+    while r <= r_final and g <= g_final and b <= b_final:
+        r += r_inc
+        g += g_inc
+        b += b_inc
+        np.fill((int(r), int(g), int(b)))
+        np.show()
+        time.sleep(increment)
 touch_pad = board.A1
 touch = touchio.TouchIn(touch_pad)
 led = dio.DigitalInOut(board.LED)
-led.direction = dio.Direction.OUTPUT 
-my_list = [red, green, blue, yellow]
+led.direction = dio.Direction.OUTPUT
+my_list.append(random.randint(0, 4))
 
 A1 = board.AI
 A2 = board.A2
@@ -27,5 +56,4 @@ Touch3 = touchio.TouchIn(A3)
 Touch4 = touchio.TouchIn(A4)
 
 while True:
-  
-  print(my_list)
+    print(my_list)
